1682b4c1970ccb2bcca88adae8d5c4dd840fbca1
leonakao <leonakao.dev@gmail.com>
2022-10-24 18:15:10 -0300

    feat: add log

--- src/app/tools/encrypter.service.ts
+++ src/app/tools/encrypter.service.ts
@@ -2,6 +2,8 @@ import { IEncrypterService } from "../signup.service";
 
 export class FakeEncrypterService implements IEncrypterService{
     async encrypt(value: string): Promise<string> {
+        console.log(`Encrypting value ${value}`);
+        
         return `${value}.encrypted`;
     }
 }
